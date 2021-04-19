b2ed4c5bc6bcd082a2f4a636f30760dea27c3a48
dylan2mc <82774727+dylan2mc@users.noreply.github.com>
2021-04-19 17:07:04 -0400

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,3 @@
+# Copyright 2021 Dylan McFarland dylanmc@bu.edu
+marijuana: marijuana.cpp
+	g++ marijuana.cpp -o marijuana -lsfml-graphics -lsfml-window -lsfml-system -lsfml-network
---
+++ marijuana.cpp
@@ -0,0 +1,142 @@
+#include<iostream>
+#include <SFML/Graphics.hpp>
+
+// Constant button dimensions
+const int bwidth = 100;
+const int bheight = 30;
+
+// Function for button pressing
+bool isMouseOver(sf::RenderWindow &window, sf::RectangleShape &button) {
+  // Mouse x and y coords
+  int mouseX = sf::Mouse::getPosition(window).x;
+  int mouseY = sf::Mouse::getPosition(window).y;
+
+  // Button Start Position coords
+  int btnPosX = button.getPosition().x;
+  int btnPosY = button.getPosition().y;
+
+  // Button End Position coords
+  int btnxPosWidth = button.getPosition().x + bwidth;
+  int btnyPosHeight = button.getPosition().y + bheight;
+
+  // Check to see if mouse is within button bounds
+  if (mouseX < btnPosX && mouseX > btnPosX - bwidth && mouseY < btnyPosHeight && mouseY > btnPosY) {
+    return true;
+  }
+  return false;
+}
+
+
+int main() {
+  // Set window and font
+  sf::RenderWindow window(sf::VideoMode(500, 500), "Marijuana");
+  sf::Font font;
+  font.loadFromFile("/usr/share/fonts/truetype/ubuntu/Ubuntu-BI.ttf");
+
+  // Title declarations
+  sf::Text title;
+  title.setFont(font);
+  title.setFillColor(sf::Color::White);
+  title.setOutlineColor(sf::Color(39, 121, 21));
+  title.setOutlineThickness(1);
+  title.setPosition(145, 10);
+
+  // History button and label declarations
+  sf::RectangleShape history(sf::Vector2f(bheight, bwidth));
+  history.setPosition(150, 130);
+  history.setFillColor(sf::Color::White);
+  history.setRotation(90);
+  sf::Text histlabel;
+  histlabel.setFont(font);
+  histlabel.setFillColor(sf::Color(39, 121, 21));
+  histlabel.setOutlineColor(sf::Color::Black);
+  histlabel.setOutlineThickness(0.2);
+  histlabel.setPosition(150+15-bwidth, 130+5); // Weird numbers for formatting
+
+  // Conviction Erasure button and label declarations
+  sf::RectangleShape convict(sf::Vector2f(bheight, bwidth));
+  convict.setPosition(450, 130);
+  convict.setFillColor(sf::Color::White);
+  convict.setRotation(90);
+  sf::Text convlabel;
+  convlabel.setFont(font);
+  convlabel.setFillColor(sf::Color(39, 121, 21));
+  convlabel.setOutlineColor(sf::Color::Black);
+  convlabel.setOutlineThickness(0.2);
+  convlabel.setPosition(450+2-bwidth, 130+10); // Weird numbers for formatting
+
+  // Repeat button and label declarations using above template as needed
+
+  // Framerate limit and declaring event for use
+  window.setFramerateLimit(5);
+  sf::Event event;
+
+  // Window operations
+  while (window.isOpen()) {
+    // Set all objects into SFML
+    title.setString("Marijuana Info");
+    window.draw(title);
+    window.draw(history);
+    histlabel.setString("History");
+    histlabel.setCharacterSize(20);
+    window.draw(histlabel);
+    window.draw(convict);
+    convlabel.setString("Conviction Erasure");
+    convlabel.setCharacterSize(11);
+    window.draw(convlabel);
+    window.display();
+    window.clear(sf::Color(137, 197, 125));
+
+    // Check for events (window closing or button pushing)
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // Close window/program on exit click (top right 'x')
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+
+      // Button click events
+      if (sf::Event::MouseButtonPressed) {
+
+        // History button click
+        if (isMouseOver(window, history)) {
+          if (event.mouseButton.button == sf::Mouse::Left) {
+            std::cout << "Click\n";
+            while(window.isOpen()) {
+              window.clear(sf::Color(137, 197, 125));
+              sf::Text test;
+              test.setFont(font);
+              test.setFillColor(sf::Color::White);
+              test.setOutlineColor(sf::Color(39, 121, 21));
+              test.setOutlineThickness(1);
+              test.setPosition(80, 10);
+              test.setString("This would be info \nabout button clicked!!!");
+              window.draw(test);
+              window.display();
+
+              // Check for events (window closing or button pushing)
+              while (window.pollEvent(event)) {
+                // Close window/program on exit click (top right 'x')
+                if (event.type == sf::Event::Closed) {
+                window.close();
+                }
+                // Put in main page button click action here
+              }
+            }
+          }
+        }
+
+        // Conviction erasure button click
+        if (isMouseOver(window, convict)) {
+          if (event.mouseButton.button == sf::Mouse::Left) {
+            // Conviction erasure info page is displayed
+            std::cout << "Button was Clicked!\n";
+          }
+        }
+
+        // Repeat above setup for all button objects
+      }
+    }
+  }
+  return 0;
+}
